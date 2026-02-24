91d9cf1db63485e9245223c379d4efe60d140b99
Angel Scatman <angelscatman@hotmail.com>
2026-02-23 20:55:40 -0600

    refactor(section): simplificar clase Animal, constructor y actualizar tsconfig con erasable false

--- sections/01-intro-typescript/src/bases/03-classes.ts
+++ sections/01-intro-typescript/src/bases/03-classes.ts
@@ -1,18 +1,12 @@
 
 export class Animal {
 
-    public id: number;
-    public name: string;
-    public species: string;
-
     // Usamos el constructor para la inicialización de las propiedades de la clase
-    // Forma tradicional y explicita de definirlo
-    constructor(id: number, name: string, species: string) {
-        console.log('Creating an animal...');
-        this.id = id;
-        this.name = name;
-        this.species = species;
-    }
+    constructor(
+        public readonly id: number, 
+        public name: string, 
+        public species: string
+    ) {}
 }
 
 export const cat = new Animal(1, 'Cat', 'Feline');
