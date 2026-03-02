da98352cf0fd81f9ce625f5638772352a1b6df5e
Angel Scatman <angelscatman@hotmail.com>
2026-03-02 16:11:22 -0600

    feat(plant): implementar CRUD con resource de nest

--- sections/03-root-directory/src/app.module.ts
+++ sections/03-root-directory/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PlantModule } from './plant/plant.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PlantModule,
   ],
   controllers: [],
   providers: [],
