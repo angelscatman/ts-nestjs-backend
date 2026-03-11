504b3c147098cb0561012c7cb9ff097ec4b4bcc8
Angel Scatman <angelscatman@hotmail.com>
2026-03-11 01:06:54 -0600

    refactor(taxonomia):  actualizar el moduio para usar CommonModule y remover el uso de la dependencia ExceptionHandlerService

--- sections/03-root-directory/src/taxonomia/taxonomia.module.ts
+++ sections/03-root-directory/src/taxonomia/taxonomia.module.ts
@@ -3,13 +3,14 @@ import { TaxonomiaService } from './taxonomia.service';
 import { TaxonomiaController } from './taxonomia.controller';
 import { Taxonomia, TaxonomiaSchema } from './entities/taxonomia.entity';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ExceptionHandlerService } from '../common/services/exception-handler.service';
+import { CommonModule } from '../common/common.module';
 
 @Module({
   controllers: [TaxonomiaController],
-  providers: [TaxonomiaService, ExceptionHandlerService],
+  providers: [TaxonomiaService],
   imports: [
-    MongooseModule.forFeature([{ name: Taxonomia.name, schema: TaxonomiaSchema }])
+    CommonModule,
+    MongooseModule.forFeature([{ name: Taxonomia.name, schema: TaxonomiaSchema }]),
   ]
 })
 export class TaxonomiaModule {}
